b19d6e96ebd3f4436c5a6ab411bbb92932068252
GausalyaaM <gausalyaamathi19@gmail.com>
2021-09-28 10:58:10 +0530

    initial commit

--- src/com/company/ga/gd.java
+++ src/com/company/ga/gd.java
@@ -1,4 +1,9 @@
 package com.company.ga;
 
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 public class gd {
+    public static void main(String[] args) {
+        System.out.println("gausy");
+    }
 }
